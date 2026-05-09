b33d778f8ba628c77cf506c74162a062c65a78f9
Nguyen Tho Duc Anh <anhntd2@msb.com.vn>
2026-05-09 10:01:21 +0700

    feat: Add @react-native/new-app-screen dependency and update ESLint configuration

--- apps/mobile/.eslintrc.js
+++ apps/mobile/.eslintrc.js
@@ -1,4 +1,11 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  parserOptions: {
+    requireConfigFile: false,
+  },
+  rules: {
+    // Disable deprecated rules that were removed in RN 0.85.3
+    '@react-native/no-deep-imports': 'off',
+  },
 };
